6d1475f14adb0dc275ad7d0b786d7f0113ec3eff
ibrahim Sağıroğlu <ibrahimsag@users.noreply.github.com>
2016-10-28 12:49:46 +0300

    cleanup connections

--- lib/health_check/redis_health_check.rb
+++ lib/health_check/redis_health_check.rb
@@ -6,7 +6,9 @@ module HealthCheck
       unless defined?(::Redis)
         raise "Wrong configuration. Missing 'redis' gem"
       end
-      res = ::Redis.new(url: ENV["REDIS_URL"], password: ENV["REDIS_PASS"]).ping
+      cli = ::Redis.new(url: ENV["REDIS_URL"], password: ENV["REDIS_PASS"])
+      res = cli.ping
+      cli.quit
       res == 'PONG' ? '' : "Redis.ping returned #{res.inspect} instead of PONG"
     rescue Exception => e
       create_error 'redis', e.message
